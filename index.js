5eef3e34a97e2a209cb5bbb6294cba3277c71359
Marcel Walk <walk.marcel.97@googlemail.com>
2020-09-28 14:15:22 +0200

    Started commenting removed repos folder

--- index.js
+++ index.js
@@ -7,15 +7,17 @@ let server = new gitserver.GitServer();
 server.start(7005);
 
 var app = express();
+
+//For static ressources (vue.js)
 app.use(express.static(__dirname));
-// respond with "hello world" when a GET request is made to the homepage
+
 app.get('/', function (req, res) {
     res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
+//API reruest=
 app.get('/repos', async function (req, res) {
     var result = server.getRepos((err, results) => {
-        console.log(results);
         res.send(results);
     });
 });
